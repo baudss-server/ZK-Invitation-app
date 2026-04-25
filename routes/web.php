bbe16c06ce3b9ec3daf55767cdae363e24a2bf2d
Baudss_dev <jerw3345@gmail.com>
2026-04-25 23:59:05 +0800

    web

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvitationController;
+
+// Nilagyan natin ng pangalan itong route!
+Route::get('/invitation', function () {
+    return view('invitation');
+})->name('invitation');
+
+Route::post('/rsvp', [InvitationController::class, 'store'])->name('rsvp.submit');
+
+Route::get('/invitation-admin', [InvitationController::class, 'admin'])->name('invitation.admin');
+Route::get('/invitation-admin/rsvps', [InvitationController::class, 'adminRsvps'])->name('invitation.admin.rsvps');
+
+Route::get('/event-details', function () {
+    return view('details');
+})->name('event.details');
